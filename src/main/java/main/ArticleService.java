7a0a800aae633ce92983175930f02ea80f0eadf2
spurthinaveli98 <spurthinaveli98@gmail.com>
2019-02-19 14:52:29 +0530

    SpringAssignment

---
+++ src/main/java/main/ArticleService.java
@@ -0,0 +1,49 @@
+package main;
+
+import model.Article;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class ArticleService {
+
+    List<Article> articleList = new ArrayList<Article>(Arrays.asList(
+            new Article("1","Article1","Description01"),
+            new Article("2","Article2","Description02"),
+            new Article("3","Article3","Description03")
+    ));
+
+    public List<Article> getAllArticles()
+    {
+        return articleList;
+    }
+    public Article getArticle(String id)
+    {
+        return articleList.stream().filter(t-> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addArticle(Article article) {
+        articleList.add(article);
+    }
+
+
+    public void updateArticle(Article art, String id) {
+
+        for(int i=0;i<articleList.size();i++)
+        {
+            Article article =articleList.get(i);
+            if(article.getId().equals(id))
+            {
+                 articleList.set(i,art);
+            }
+        }
+    }
+
+    public void deleteArticle(String id) {
+        articleList.removeIf(t->t.getId().equals(id));
+    }
+}
